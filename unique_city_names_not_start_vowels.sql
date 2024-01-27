3a1c4635e2cf6349e228eeb042ddc6643c080961
Chris Forbes <u1151925@umail.usq.edu.au>
2024-01-27 23:51:46 +1000

    Solved more HackerRank problems

---
+++ unique_city_names_not_start_vowels.sql
@@ -0,0 +1,5 @@
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE 'A%' AND CITY NOT LIKE 'E%' AND CITY NOT LIKE 'I%' AND CITY NOT LIKE 'O%' AND CITY NOT LIKE 'U%';
